bb3e8a7501378671183cce86083bc27f66ac189a
Richard Wilson <rich@extream.app>
2020-10-31 01:06:36 +0000

    send message to all users if from a moderator or public is true

--- server.js
+++ server.js
@@ -282,6 +282,9 @@ const actions = {
             } else if (data.requester) {
               // this is a reply to the sender
               io.to(data.requester.id).emit('consumer_chat_receive', { id, ...data });
+            } else if (!data.parent && data.moderators.includes(user.id) || data.private === false) {
+              // send to all as its a global message from the moderators or public
+              io.to(id).emit('consumer_chat_receive', { id, ...data });
             }
           } else {
             io.to(id).emit('consumer_chat_receive', { id, ...data });
